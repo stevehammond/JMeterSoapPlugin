834e4f45b57deb24f6253b71b6e6c100475a6524
Mark Fink <mark@mark-fink.de>
2012-06-03 20:13:39 +0200

    little cleanup

--- demo_client/src/org/testingsoftware/client/WebserviceClient.java
+++ demo_client/src/org/testingsoftware/client/WebserviceClient.java
@@ -1,13 +1,5 @@
 package org.testingsoftware.client;
-/*
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-*/
+
 import java.io.StringReader;
 
 import javax.xml.transform.stream.StreamResult;
@@ -36,11 +28,7 @@ public class WebserviceClient extends WebServiceGatewaySupport {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("webservice-config.xml");
         WebserviceClient client = (WebserviceClient) context.getBean("webServiceClient");
- 
-        // ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-        //     "webservice-config.xml");
-        // WebserviceClient client = (WebserviceClient) context.getBean(
-        //     "webServiceTemplate");
+
         if (args.length != 0) {
             System.out.println("does not process any arguments!");
             System.exit(2);
@@ -55,60 +43,6 @@ public class WebserviceClient extends WebServiceGatewaySupport {
         StreamSource source = new StreamSource(new StringReader(msg));
         StreamResult result = new StreamResult(System.out);
 
-        //webServiceTemplate.sendSourceAndReceiveToResult(
-        // "http://localhost:8080/demo_server.0.1/calc-service",
-        // source, result);
         getWebServiceTemplate().sendSourceAndReceiveToResult(source, result);
     }
 }
-
-/*
-
-
-
-
-
-
-
-
-
-
-import java.io.StringReader;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
-
-import org.springframework.ws.WebServiceMessageFactory;
-import org.springframework.ws.client.core.WebServiceTemplate;
-import org.springframework.ws.transport.WebServiceMessageSender;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-public class WebserviceClient {
-
-    private static final String MESSAGE =
-        "<message xmlns=\"http://tempuri.org\">Hello Web Service World</message>";
-
-    private final WebServiceTemplate webServiceTemplate = new WebServiceTemplate();
-
-    // send to an explicit URI
-    public void sendAndReceive() {
-        StreamSource source = new StreamSource(new StringReader(MESSAGE));
-        StreamResult result = new StreamResult(System.out);
-        webServiceTemplate.sendSourceAndReceiveToResult(
-            "http://localhost:8080/demo_server.0.1/calc-service",
-            source, result);
-    }
-
-    public static void main(String[] args) {
-
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-            "webservice-config.xml");
-        WebserviceClient client = (WebserviceClient) context.getBean(
-            "webServiceTemplate");
-        if (args.length != 0) {
-            System.out.println("does not process any arguments!");
-            System.exit(2);
-        }
-        client.sendAndReceive();
-    }
-}
-*/
